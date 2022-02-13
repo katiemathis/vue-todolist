e1d003768e855cd3f49376d297119e48c5624167
katiepalumbo <palumbo.katie@gmail.com>
2022-02-13 22:47:54 +0100

    esercizio completo tranne strikethrough

--- main.js
+++ main.js
@@ -2,7 +2,42 @@ const app = new Vue (
     {
         el: '#app',
         data: {
-            newtodo: ''
+            newtodo: '',
+            todos: [
+                'pick up tomatoes',
+                'buy cherries',
+                'finish homework',
+                'order new running shoes'
+            ],
+            doneClass: '',
+            done: false,
+        },
+        methods: {
+            addToDo() {
+                this.todos.push(this.newtodo);
+                this.newtodo = '';
+            },
+            removeToDo(index) {
+                this.todos.splice(index, 1);
+            },
+            doneTask(index) {
+                if (this.done == false) {
+                    this[index]this.doneClass = 'doneClass';
+                    this.done = true;
+                } else {
+                    this[index]this.doneClass = ''
+                }
+
+            }
+
         }
+
     }
 )
+
+
+/*if (this.done == false) {
+    this.doneClass.splice(index, 1) = 'doneClass';
+} else {
+    
+}*/
